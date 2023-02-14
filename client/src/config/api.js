55b5c78e6057e257d83f5339da6cf483338a93e6
jojorda <115984161+jojorda@users.noreply.github.com>
2023-02-14 10:56:29 +0700

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://deployyy-production-clear.up.railway.app/api/v1/",
+  baseURL: "https://depoly-production.up.railway.app/api/v1/",
 });
 
 export const setAuthToken = (token) => {
